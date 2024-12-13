a66bb246d6af77213d86c6936ce3b43d7e07c216
Sourabh birada <officialsourabhchoudhary@gmail.com>
2024-12-14 00:22:09 +0530

    update frontend url

--- Frontendgithub/src/components/user/profile.jsx
+++ Frontendgithub/src/components/user/profile.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from 'react-router-dom';
-import "../../style/Profile.css"; // Ensure to import the CSS file
+import "../../style/profile.css"; 
 
 const Profile = () => {
   const [user, setUser] = useState(null);
